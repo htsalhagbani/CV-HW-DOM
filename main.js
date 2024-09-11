76f0c68ec92a4407c0d195e538b0e330ab862668
Haneen <htsalhagbani@gmail.com>
2024-09-12 02:23:32 +0300

    add solution

---
+++ main.js
@@ -0,0 +1,134 @@
+document.getElementsByClassName('logo-continer')[0].onmouseover = function() {
+    this.classList.add('hover-active');
+};
+
+document.getElementsByClassName('logo-continer')[0].onmouseout = function() {
+    this.classList.remove('hover-active');
+};
+
+
+document.getElementsByClassName('logo-continer-small')[0].onmouseover = function() {
+    this.classList.add('hover-active');
+};
+
+document.getElementsByClassName('logo-continer-small')[0].onmouseout = function() {
+    this.classList.remove('hover-active');
+};
+
+
+document.getElementsByClassName('phone')[0].onmouseover = function() {
+    this.classList.add('hover-active');
+};
+
+document.getElementsByClassName('phone')[0].onmouseout = function() {
+    this.classList.remove('hover-active');
+};
+
+document.getElementsByClassName('email')[0].onmouseover = function() {
+    this.classList.add('hover-active');
+};
+
+document.getElementsByClassName('email')[0].onmouseout = function() {
+    this.classList.remove('hover-active');
+};
+
+document.getElementsByClassName('loc')[0].onmouseover = function() {
+    this.classList.add('hover-active');
+};
+
+document.getElementsByClassName('loc')[0].onmouseout = function() {
+    this.classList.remove('hover-active');
+};
+
+document.getElementsByClassName('web')[0].onmouseover = function() {
+    this.classList.add('hover-active');
+};
+
+document.getElementsByClassName('web')[0].onmouseout = function() {
+    this.classList.remove('hover-active');
+};
+
+document.getElementsByClassName('EDUCATION')[0].onmouseover = function() {
+    this.classList.add('hover-active');
+};
+
+document.getElementsByClassName('EDUCATION')[0].onmouseout = function() {
+    this.classList.remove('hover-active');
+};
+
+document.getElementsByClassName('skills')[0].onmouseover = function() {
+    this.classList.add('hover-active');
+};
+
+document.getElementsByClassName('skills')[0].onmouseout = function() {
+    this.classList.remove('hover-active');
+};
+
+
+document.getElementsByClassName('language')[0].onmouseover = function() {
+    this.classList.add('hover-active');
+};
+
+document.getElementsByClassName('language')[0].onmouseout = function() {
+    this.classList.remove('hover-active');
+};
+
+//
+document.getElementsByClassName('NameHeader')[0].onmouseover = function() {
+    this.classList.add('hover-active');
+};
+
+document.getElementsByClassName('NameHeader')[0].onmouseout = function() {
+    this.classList.remove('hover-active');
+};
+
+document.getElementsByClassName('profile')[0].onmouseover = function() {
+    this.classList.add('hover-active');
+};
+
+document.getElementsByClassName('profile')[0].onmouseout = function() {
+    this.classList.remove('hover-active');
+};
+
+document.getElementsByClassName('timeline-content')[0].onmouseover = function() {
+    this.classList.add('hover-active');
+};
+
+document.getElementsByClassName('timeline-content')[0].onmouseout = function() {
+    this.classList.remove('hover-active');
+};
+
+
+
+document.getElementsByClassName('timeline')[0].onmouseover = function() {
+    this.classList.add('hover-active');
+};
+
+document.getElementsByClassName('timeline')[0].onmouseout = function() {
+    this.classList.remove('hover-active');
+};
+
+document.getElementsByClassName('refrence1')[0].onmouseover = function() {
+    this.classList.add('hover-active');
+};
+
+document.getElementsByClassName('refrence1')[0].onmouseout = function() {
+    this.classList.remove('hover-active');
+};
+
+document.getElementsByClassName('refrence')[0].onmouseover = function() {
+    this.classList.add('hover-active');
+};
+
+document.getElementsByClassName('refrence')[0].onmouseout = function() {
+    this.classList.remove('hover-active');
+};
+
+
+document.getElementsByClassName('refrence-Table')[0].onmouseover = function() {
+    this.classList.add('hover-active');
+};
+
+document.getElementsByClassName('refrence-Table')[0].onmouseout = function() {
+    this.classList.remove('hover-active');
+};
